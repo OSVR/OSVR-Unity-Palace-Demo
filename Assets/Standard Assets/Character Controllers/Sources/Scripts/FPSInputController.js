0c8d10fc843d491285ce70adc6205f05aa5bcc91
Greg Aring <greg.aring@gmail.com>
2015-06-15 01:05:03 -0400

    Updated input controls.

--- Assets/Standard Assets/Character Controllers/Sources/Scripts/FPSInputController.js
+++ Assets/Standard Assets/Character Controllers/Sources/Scripts/FPSInputController.js
@@ -9,13 +9,14 @@ function Awake () {
 // Update is called once per frame
 function Update () {
 	// Get the input vector from keyboard or analog stick
-	var directionVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-	
+    var directionVector = Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 	if (directionVector != Vector3.zero) {
 		// Get the length of the directon vector and then normalize it
 		// Dividing by the length is cheaper than normalizing when we already have the length anyway
 		var directionLength = directionVector.magnitude;
-		directionVector = directionVector / directionLength;
+	    directionVector = directionVector / directionLength;
+	    if (directionVector.sqrMagnitude > 1)
+	        directionVector.Normalize();
 		
 		// Make sure the length is no bigger than 1
 		directionLength = Mathf.Min(1, directionLength);
@@ -28,8 +29,11 @@ function Update () {
 		directionVector = directionVector * directionLength;
 	}
 	
-	// Apply the direction to the CharacterMotor
-	motor.inputMoveDirection = camDirection.rotation * directionVector;
+    // Apply the direction to the CharacterMotor
+    var theForwardDirection = camDirection.TransformDirection (Vector3.forward);
+    theForwardDirection.y = 0;
+    theForwardDirection.Normalize ();
+    motor.inputMoveDirection = camDirection.rotation * directionVector;
 	motor.inputJump = Input.GetButton("Jump");
 }
 
